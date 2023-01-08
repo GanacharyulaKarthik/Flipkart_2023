c13047967a313267ed63e1ea69f60d8c0d2a50f7
KarthikKumar <karthikganachari@gmail.com>
2023-01-08 07:39:30 +0530

    In the Flipkart mobiles and clothing code developed by developer

---
+++ Clothing/src/main/java/com/ssrs/Puma/Allen SollyPlugin.java
@@ -0,0 +1,19 @@
+package com.ssrs.Puma;
+
+import com.ssrs.framework.extend.plugin.AbstractPlugin;
+import com.ssrs.framework.extend.plugin.PluginException;
+
+/**
+ * @author ssrs
+ */
+public class PumaPlugin extends AbstractPlugin {
+    @Override
+    public void start() throws PluginException {
+
+    }
+
+    @Override
+    public void stop() throws PluginException {
+
+    }
+}
